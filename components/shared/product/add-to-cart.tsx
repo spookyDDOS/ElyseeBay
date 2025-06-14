2537725509b39ac7a13b208c3b7a9f46581c1404
spookyDDOS <dobos.b.david@gmail.com>
2025-06-14 22:05:24 +0300

    remove from cart, dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,12 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { Plus, Minus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { toast } from "sonner";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
   const handleAddToCart = async () => {
@@ -28,7 +28,34 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Handle remove from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (res.success) {
+      toast(res.message);
+    } else {
+      toast.error(res.message);
+    }
+
+    return;
+  };
+
+  // Check is item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus /> Add To Cart
     </Button>
